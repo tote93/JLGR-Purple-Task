fec2cdfc47d61cd3eedc31ab7c9358b508b346ab
Jose Luis <joseluis_12193@hotmail.com>
2020-09-22 18:13:18 +0200

    Added Base project
    - App has been cleaned
    - Deletion of non-neccesary files

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      {/* Header */}
+      {/* Home */}
+      {/* Join-us */}
+      {/* About */}
+      {/* Info */}
+      {/* Legal Text */}
+      {/* Footer */}
+    </div>
+  );
+}
+
+export default App;
